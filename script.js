3b55ab0988e4de895ce548f5edb970ebf58dce98
Alex Dandamudi <alexdandamudi5@gmail.com>
2025-07-23 01:35:11 +0530

    worked on profile section
    implemented typewriter animation to the text

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+<script>
+    var typed = new Typed("".auto-type",{
+        strings: ["Frontend Developer", "ML Engineer", "Software Developer"],
+        typeSpeed: 150;
+        backSpeed: 150;
+        loop: true)
+    }
+</script>
